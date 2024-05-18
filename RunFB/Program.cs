f1c08cc7fc592238bfdd5655931cf7119b13bb76
Vladislav07 <belov_vlad@list.ru>
2024-05-18 17:20:43 +0300

    init

---
+++ RunFB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FullBomHoum;
+
+namespace RunFB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAssemblyID ass = new GetAssemblyID();
+            ass.OnCmd();
+        }
+    }
+}
